fadf487d47edf88e9732c9c34cfb1742c52e4a19
Amed-Ababneh <63665874+Amed-Ababneh@users.noreply.github.com>
2025-05-19 20:23:51 -0500

    Initial commit

---
+++ Chapters/Chapter1/challange_two.py
@@ -0,0 +1,4 @@
+season = input('Enter Season of the year')
+number = input('Enter a number')
+adjective = input('Enter an adjective')
+print("On a " + adjective + " " + season + " day, I drink at least " + number + " cups of juice!")
